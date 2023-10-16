08fa740b048b2e6bcf97c4516d0ed61af655a6e0
Manikant Singh <112557206+manikant2869@users.noreply.github.com>
2023-10-16 19:09:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ server.use("/metrics",async(req,res)=>{
 server.listen(PORT,(error)=>{
     if(!error){
 
-      console.log("Server is Successfully Running,and App is listening on port "+ PORT)
+      console.log("Server is Successfully Running and App is listening on port "+ PORT)
     }
     else{
       console.log("Error occurred, server can't start", error);
